4df192006f3b97ee32add90d625ca3c4459a5cca
Anthony Eli Agbenu <lefootballroi@yahoo.com>
2014-02-24 11:10:09 +0000

    added the login and logout...

--- ribbit_eli/urls.py
+++ ribbit_eli/urls.py
@@ -7,6 +7,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ribbit_eli.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$','ribbit_eli_app.views.index'),
+    url(r'^login$','ribbit_eli_app.views.login_view'),
+    url(r'^logout$','ribbit_eli_app.views.logout_view'),
+    url(r'^signup$','ribbit_eli_app.views.signup'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- ribbit_eli_app/views.py
+++ ribbit_eli_app/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
 
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from ribbit_app.forms import AuthenticateForm, UserCreateForm, RibbitForm
+
 # Create your views here.
+
+def index(request,auth_form=None,user_form=None):
+	#if logged in 
+	if request.user.is_authenticated():
+		ribbit_form = RibbitForm()
+		user = request.user
+		ribbit_self = Ribbit.objects.filter(user=user.id)
+		ribbits_budddies = Ribbit.objects.filter(user__userprofile__in=user.profile.all)
+		ribbits = ribbits_self | ribbits_budddies
+
+		return render(request, 
+					  'buddies.html',
+					  {'ribbit_form':ribbit_form,'user':user,
+					   'ribbits':ribbits,
+					   'next_url':'/',})
+
+	else:
+		#User is not logged in
+		auth_form = auth_form or AuthenticateForm()
+		user_form = user_form or UserCreateForm()
+
+		return render(request,'home.html',
+					   {'auth_form':auth_form,'user_form':user_form, })
+
+
+def login_view(request):
+	if request.method == 'POST':
+		form = AuthenticateForm(data=request.POST)
+		if form.is_valid():
+			login(request,form.get_user())
+			#Success
+			return redirect('/')
+		else:
+			#failure
+			return index(request,auth_form=form)
+	return redirect('/')
+
+
+def logout_view(request):
+	logout(request)
+	return redirect('/')
